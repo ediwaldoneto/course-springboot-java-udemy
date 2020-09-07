51e10c81d760d9cf0d466769e20fa4035c1a24d1
ediwaldoneto <ediwaldo.neto@outlook.com>
2020-09-07 16:44:24 -0300

    Product

---
+++ src/main/java/br/com/project/services/ProductService.java
@@ -0,0 +1,26 @@
+package br.com.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.project.entities.Product;
+import br.com.project.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> product = productRepository.findById(id);
+		return product.get();
+	}
+}

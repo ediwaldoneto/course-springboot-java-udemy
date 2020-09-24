dae4961eaef28e8d4251257571b1d33a083e6d13
ediwaldoneto <ediwaldo.neto@outlook.com>
2020-09-24 16:41:18 -0300

    User insert

--- src/main/java/br/com/project/resources/UserResource.java
+++ src/main/java/br/com/project/resources/UserResource.java
@@ -1,13 +1,16 @@
 package br.com.project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.project.entities.User;
 import br.com.project.services.UserService;
@@ -29,6 +32,16 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User list = userService.findById(id);
 		return ResponseEntity.ok().body(list);
+
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(User user) {
+		user = userService.insert(user);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(user);
 
 	}
 }
